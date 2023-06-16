7480ad0e514a59cc9d194a8964157088955d49a1
Masud Rana <masud1010rana0101@gmail.com>
2023-06-16 11:20:31 -0700

    ok

--- resources/views/users/pages/home/components/slider.blade.php
+++ resources/views/users/pages/home/components/slider.blade.php
@@ -24,80 +24,28 @@
     background-size: cover;
     background-position: center;
 }
+.swiper-slide img{
+}
 </style>
 
-<div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff;" class="swiper mySwiper">
-    <div class="parallax-bg" style="background-image: url(https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg)" data-swiper-parallax="-23%"></div>
+  <!-- Swiper -->
+  <div class="swiper mySwiper">
     <div class="swiper-wrapper">
-        
-        <div class="swiper-slide">
-            <div class="main-header" data-swiper-parallax="-300">Slide 2</div>
-            <div class="header" data-swiper-parallax="-200">Subtitle</div>
-            <div class="title" data-swiper-parallax="-100">
-                <p>
-                    Lorem ipsum dolor sit amet, consectet
-                </p>
-            </div>
-        </div>
-
-        <div class="swiper-slide">
-            <div class="main-header" data-swiper-parallax="-300">Slide 2</div>
-            <div class="header" data-swiper-parallax="-200">Subtitle</div>
-            <div class="title" data-swiper-parallax="-100">
-                <p>
-                    Lorem ipsum dolor sit amet, consectet
-                </p>
-            </div>
-        </div>
-
-        <div class="swiper-slide">
-            <div class="main-header" data-swiper-parallax="-300">Slide 2</div>
-            <div class="header" data-swiper-parallax="-200">Subtitle</div>
-            <div class="title" data-swiper-parallax="-100">
-                <p>
-                    Lorem ipsum dolor sit amet, consectet
-                </p>
-            </div>
-        </div>
-
-        <div class="swiper-slide">
-            <div class="main-header" data-swiper-parallax="-800">Slide 2</div>
-            <div class="header" data-swiper-parallax="-500">Subtitle</div>
-            <div class="title" data-swiper-parallax="-200">
-                <p>
-                    Lorem ipsum dolor sit amet, consectet
-                </p>
-            </div>
-        </div>
-
-        <div class="swiper-slide">
-            <div class="main-header" data-swiper-parallax="-300">Slide 2</div>
-            <div class="header" data-swiper-parallax="-200">Subtitle</div>
-            <div class="title" data-swiper-parallax="-100">
-                <p>
-                    Lorem ipsum dolor sit amet, consectet
-                </p>
-            </div>
-        </div>
-        
+      <div class="swiper-slide"><img src="{{asset("images\slider\sl1.png")}}" alt=""></div>
+      <div class="swiper-slide"><img src="{{asset("images\slider\sl2.jpg")}}" alt=""></div>
+      <div class="swiper-slide"><img src="{{asset("images\slider\sl3.jpg")}}" alt=""></div>
+      <div class="swiper-slide"><img src="{{asset("images\slider\sl4.jpg")}}" alt=""></div>
     </div>
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
     <div class="swiper-pagination"></div>
-</div>  
+  </div>
+
+<!-- Swiper JS -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<!-- Initialize Swiper -->
 <script>
-    // slider st 
     var swiper = new Swiper(".mySwiper", {
-        speed: 600,
-        parallax: true,
         pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+        el: ".swiper-pagination",
         },
     });
 </script>
